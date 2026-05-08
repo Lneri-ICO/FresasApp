8d29e446f2bfced28f3fdebbf9965fdd26757fb1
Lneri-ICO <59067leo@universidaddeleon.edu.mx>
2026-05-08 13:05:33 -0600

    Segundo commit

--- app/src/main/java/com/example/fresasapp/ui/SplashScreen.kt
+++ app/src/main/java/com/example/fresasapp/ui/SplashScreen.kt
@@ -2,18 +2,22 @@ package com.example.fresasapp.ui
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fresasapp.R
 import kotlinx.coroutines.delay
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.ui.Alignment
 
 @Composable
 fun SplashScreen(onSplashTerminado: () -> Unit) {
@@ -31,18 +35,20 @@ fun SplashScreen(onSplashTerminado: () -> Unit) {
         onSplashTerminado()
     }
 
+    // El fondo es del mismo color rosa del logo para que se vea uniforme
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFE5174E))
+            .background(Color(0xFFF52E65)), // mismo rosa del logo
+        contentAlignment = Alignment.Center
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo_splash),
             contentDescription = "Nay&Jos",
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .alpha(alpha),
-            contentScale = ContentScale.Crop  // ← Crop para llenar sin bordes
+            contentScale = ContentScale.FillWidth
         )
     }
 }
